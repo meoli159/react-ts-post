85d3cd7c64da6d3d811b7ffe91f4cec48d020a1f
Cyrus <kur0dk0@gmail.com>
2023-09-22 14:20:00 +0700

    fix api

--- client/src/components/PostsList.tsx
+++ client/src/components/PostsList.tsx
@@ -20,7 +20,7 @@ function PostsList({ isPosting, onStopPosting }: IProps) {
   useEffect(() => {
     setIsFetching(true);
     async function fetchPosts() {
-      const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}api/posts`);
+      const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/posts`);
       const resData = await res.json();
       setPost(resData.data);
       setIsFetching(false);
